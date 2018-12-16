a7dee595521af8104fd09a0588d62f582175f9e1
farrahchan <45841255+farrahchan@users.noreply.github.com>
2018-12-16 17:21:46 +0800

    解析影片信息

--- src/com/chan/spider11/App.java
+++ src/com/chan/spider11/App.java
@@ -5,6 +5,8 @@ public class App {
 	 // alt+/
 	public static void main(String[] args) {
 		
+				Element t= e.select(".title").first();
+				String num = e.select(".star span").last().text();
 	}
 
 }
---
+++ src/com/chan/spider11/Film.java
@@ -0,0 +1,47 @@
+package com.chan.spider11;
+/**
+ * 
+ * @author Chan
+ *
+ */
+
+public class Film {
+	
+	/**
+	 * 影片名称
+	 */
+	String title;
+	
+	/**
+	 * 相关信息
+	 */
+	String info;
+	
+	/**
+	 * 评分
+	 */
+	String rating;
+	
+	/**
+	 * 评分人数
+	 */
+	int num;
+	
+	/**
+	 * 影片排名
+	 */
+	int id;
+	
+	/**
+	 * 海报 
+	 */
+	String poster;
+	
+	/**
+	 * 
+	 */
+	String quote;
+	
+	
+
+}
